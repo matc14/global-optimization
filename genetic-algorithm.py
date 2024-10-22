7a16f13e1bfc7fd464f92191e318e16953426733
matcze <mc84544@student.uwb.edu.pl>
2024-10-22 09:49:11 +0200

    Fixed random_number range and changed genetic_algorithm return value type to tuple

--- genetic-algorithm.py
+++ genetic-algorithm.py
@@ -1,5 +1,6 @@
 import math
 import random
+from operator import ifloordiv
 
 
 def rastrigin_function(xi):
@@ -9,23 +10,29 @@ def rastrigin_function(xi):
     return 10 * len(xi) + sum
 
 
+import random
+
 def calculate_binary_length(a, b, d):
     m_list = []
+    c_list = []
     for i in range(len(a)):
         m = 0
+        c = (b[i] - a[i]) * 10**d[i] + 1
         while 2**m < (b[i] - a[i]) * 10**d[i] or 2**(m - 1) > (b[i] - a[i]) * 10**d[i]:
             m += 1
         m_list.append(m)
-    return m_list
+        c_list.append(c)
+    return m_list, c_list
 
-
-def generate_population(n, m_list):
+def generate_population(n, m_list, c_list):
     all_populations = []
 
-    for m in m_list:
+    for i in range(len(m_list)):
+        m = m_list[i]
+        c = c_list[i]
         population = set()
         while len(population) < n:
-            random_number = random.randint(0, 2**m - 1)
+            random_number = random.randint(0, c - 1)
             binary_value = bin(random_number)[2:].zfill(m)
             population.add(binary_value)
         all_populations.append(list(population))
@@ -37,7 +44,8 @@ def generate_population(n, m_list):
 
     print(all_populations)
     print(combined_population)
-    return all_populations
+    return all_populations, combined_population
+
 
 
 def evaluate_population(populations, a_list, b_list, m_list):
@@ -65,14 +73,15 @@ def evaluate_population(populations, a_list, b_list, m_list):
 
 
 def genetic_algorithm(a, b, d):
-    m = calculate_binary_length(a, b, d)
-    population = generate_population(10, m)
-    result = evaluate_population(population, a, b, m)
-    return result
+    m, c = calculate_binary_length(a, b, d)
+    populations, chromosomes = generate_population(10, m, c)
+    values = evaluate_population(populations, a, b, m)
+    combined = list(zip(chromosomes, values))
+    return combined
 
 
 A = [-1, 1, 1, 1]
 B = [1, 3, 4, 4]
 D = [1, 1, 1, 2]
 
-print(genetic_algorithm(A, B, D))
+population = genetic_algorithm(A, B, D)
